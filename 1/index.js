9d7e619ae6cbcde9ac246e399a3a272bdee549b8
andresguastavino <andresguastavino@gmail.com>
2023-12-02 10:33:03 -0300

    day 1

---
+++ 1/index.js
@@ -0,0 +1,39 @@
+function findFirstRepeated(gifts) {
+  if (!gifts.length) {
+    return -1
+  }
+
+  let repeatedGift = -1
+  let giftsAux = [ gifts[0] ]
+
+  while (repeatedGift === -1 && giftsAux.length < gifts.length) {
+    const currentGift = gifts[giftsAux.length]
+    repeatedGift = giftsAux.includes(currentGift) ? currentGift : repeatedGift
+    giftsAux.push(currentGift)
+  }
+
+  return repeatedGift
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2]
+const firstRepeatedId = findFirstRepeated(giftIds)
+console.log(firstRepeatedId) // 3
+// Even though 2 and 3 are repeated
+// 3 appears second time first
+
+const giftIds2 = [1, 2, 3, 4]
+const firstRepeatedId2 = findFirstRepeated(giftIds2)
+console.log(firstRepeatedId2) // -1
+// It is -1 since no number is repeated
+
+const giftIds3 = [5, 1, 5, 1]
+const firstRepeatedId3 = findFirstRepeated(giftIds3)
+console.log(firstRepeatedId3) // 5
+
+const giftIds4 = [7, 1, 3, 2, 9, 7]
+const firstRepeatedId4 = findFirstRepeated(giftIds4)
+console.log(firstRepeatedId4) // 7
+
+const giftIds5 = [5, 1, 3, 2, 8, 8]
+const firstRepeatedId5 = findFirstRepeated(giftIds5)
+console.log(firstRepeatedId5) // 8
