238cc3d650660ad26e5a861c9c9525936c7fdaee
andresguastavino <andresguastavino@gmail.com>
2023-12-05 11:36:46 -0300

    optimizacion ciclomatica

--- 5/index.js
+++ 5/index.js
@@ -1,21 +1,37 @@
 function cyberReindeer(road, time) {
   let result = [ road ]
-  let steps = 0
   let roadCpy = road
-  for (let i = 1; i < time; i++) {
-    if (i === 5) {
-      roadCpy = roadCpy.replaceAll('|', '*')
-    }
-    if (roadCpy.at(steps + 1) !== '|') {
-      let roadArr = roadCpy.split('')
-      let indexOfSanta = roadArr.indexOf('S')
-      roadArr[indexOfSanta] = road.at(indexOfSanta) === '|' ? '*' : '.'
-      roadArr[indexOfSanta + 1] = 'S'
-      roadCpy = roadArr.join('')
-      steps++
-    }
+  let i = 1
+  for (; i < road.indexOf('|'); i++) {
+    let roadArr = roadCpy.split('')
+    let indexOfSanta = roadArr.indexOf('S')
+    roadArr[indexOfSanta] = '.'
+    roadArr[indexOfSanta + 1] = 'S'
+    roadCpy = roadArr.join('')
     result.push(roadCpy)
   }
+  result = result.slice(0, time)
+
+  for (; i < 5; i++) {
+    result.push(roadCpy)
+  }
+  result = result.slice(0, time)
+
+  const obj = {
+    '|': '*',
+    '.': '.'
+  }
+
+  roadCpy = roadCpy.replaceAll('|', '*')
+  for (;i < time; i++) {
+    let roadArr = roadCpy.split('')
+    let indexOfSanta = roadArr.indexOf('S')
+    roadArr[indexOfSanta] = obj[road.at(indexOfSanta)]
+    roadArr[indexOfSanta + 1] = 'S'
+    roadCpy = roadArr.join('')
+    result.push(roadCpy)
+  }
+
   return result
 }
 
@@ -23,3 +39,7 @@ const road = 'S..|...|..'
 const time = 10 // unidades de tiempo
 const result = cyberReindeer(road, time)
 console.log(result)
+
+console.log(cyberReindeer('S..|...|..', 10))
+
+console.log(cyberReindeer('S.|.', 4))
