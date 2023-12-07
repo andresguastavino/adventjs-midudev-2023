a8b0e81dddfc525a664d80ac142453793e912825
andresguastavino <andresguastavino@gmail.com>
2023-12-07 20:30:04 -0300

    optimizacion dia 7

--- 7/index.js
+++ 7/index.js
@@ -3,20 +3,30 @@ function drawGift(size, symbol) {
     return '#\n'
   }
 
-  const result = []
-  for (let i = 1; i < size - 1; i++) {
-    let line = `#${ Array(size - 2).fill(symbol).join('') }#${ Array((size + i) - (size + 1)).fill(symbol).join('') }#`
-    result[i] = `${ Array(size - (1 + i)).fill(' ').join('') }${ line }`
-    result[size * 2 - (i + 2)] = line
+  let a = ''
+  let b = ''
+  let c = ''
+  for (let i = 0; i <= size; i++) {
+    a += '#'
+    b += symbol
+    c += ' '
   }
 
-  result[size - 1] = `${ Array(size).fill('#').join('') }${ Array(size - 2).fill(symbol).join('') }#`
-  result[2 * size - 2] = Array(size).fill('#').join('')
-  result[0] = `${ Array(size - 1).fill(' ').join('') }${ result[2 * size - 2] }`
+  let top = c.substring(0, size - 1) + a.substring(0, size)+'\n'
+  let middle = a.substring(0, size) + b.substring(0, size - 2) + '#\n'
+  let bottom = ''
+  for (let i = 0; i < size - 2; i++) {
+    top += c.substring(0, size - 2 - i)
+      + '#' + b.substring(0, size - 2)
+      + '#' + b.substring(0, (size + i) - (size))
+      + '#\n'
+    bottom += '#' + b.substring(0, size - 2)
+      + '#' + b.substring(0, size - 3 - i)
+      + '#\n'
+  }
+  bottom += a.substring(0, size)+'\n'
 
-  result.push('')
-
-  return result.join('\n')
+  return top + middle + bottom
 }
 
 /*
