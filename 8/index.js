f8c27ed41c4748ac0beb79b041cfff844b8e75db
andresguastavino <andresguastavino@gmail.com>
2023-12-08 12:04:39 -0300

    Mejor sol dia 8

--- 8/index.js
+++ 8/index.js
@@ -1,18 +1,19 @@
 function organizeGifts(gifts) {
   const nums = gifts.match(/\d+/g);
-  const chars = gifts.match(/[a-zA-Z]+/g);
+  const chars = gifts.match(/[a-z]+/g);
 
   let result = ''
   for (const i of [ ...Array.from({ length: nums.length }).keys() ]) {
-    let a = Math.floor(nums[i] / 50) 
-    let b = Math.floor((nums[i] - (a * 50)) / 10) 
-    let c = Math.floor((nums[i] - (a * 50) - (b * 10)))
+    let a = nums[i] / 50
+    let b = nums[i] % 50 / 10 
+    let c = nums[i] % 10
 
     let d = ('['+chars[i]+']').repeat(a)
     let e = ('{'+chars[i]+'}').repeat(b)
     let f = ('(').repeat(c > 0)
     f += chars[i].repeat(c)
     f += (')').repeat(c > 0)
+
     result += d + e + f
   }
 
